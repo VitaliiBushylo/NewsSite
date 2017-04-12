7aff9b312afb9faf349d171f7db24723f78739f7
Vitalii Bushylo <vitalii.bushylo@globallogic.com>
2017-04-12 09:34:41 +0300

    Added news list page.

---
+++ NewsSite/Controllers/NewsListController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsSite.Models;
+
+namespace NewsSite.Controllers
+{
+    public class NewsListController : Controller
+    {
+        // GET: NewsList
+        public ActionResult Index()
+        {
+            var news = new NewsModel();
+            return View(news);
+        }
+    }
+}
---
+++ NewsSite/Models/NewsEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NewsSite.Models
+{
+    public class NewsEntry
+    {
+        public NewsEntry()
+        {
+            CreatedOn = DateTime.Now;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Uri Origin { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
